d7b3dd36d2d24187f5e678fa19dabfc90f5a184e
Wouter de Vos <wouter@codaisseur.com>
2017-07-27 11:25:57 +0200

    Finished recipes service and seeds

--- src/models/recipes.model.js
+++ src/models/recipes.model.js
@@ -4,20 +4,21 @@
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
 
-  const ingredientSchema = new mongooseClient.Schema({
+  const ingredientSchema = new Schema({
     amount: { type: String, required: false },
     name: { type: String, required: true },
     optional: { type: Boolean, required: true, 'default': false }
   });
 
-  const cookingStepSchema = new mongooseClient.Schema({
+  const cookingStepSchema = new Schema({
     cookingTime: { type: Number, required: false }, // in minutes
     title: { type: String, required: false },
     description: { type: String, required: true }
   });
 
-  const recipes = new mongooseClient.Schema({
+  const recipes = new Schema({
     title: { type: String, required: true },
     summary: { type: String, required: true },
     photo: { type: String, default: 'http://via.placeholder.com/500x180?text=No%20Image' },
@@ -27,6 +28,8 @@ module.exports = function (app) {
     cookingTime: { type: Number, required: false }, // in minutes
     ingredients: [ingredientSchema],
     cookingSteps: [cookingStepSchema],
+    likedBy: [{ type: Schema.Types.ObjectId, ref: 'users' }],
+    authorId: { type: Schema.Types.ObjectId, ref: 'users' },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
